e17101f061544e569170a0acf6fc41d8dbf6bfcb
Rakshit Diwani <rakshitdiwani@gmail.com>
2025-08-24 15:03:38 +0530

    dark mode added

---
+++ script.js
@@ -0,0 +1,10 @@
+const themeBtn = document.getElementById("themeBtn");
+
+themeBtn.addEventListener("click", () => {
+  document.body.classList.toggle("dark-mode");
+  if (document.body.classList.contains("dark-mode")) {
+    themeBtn.textContent = "☀ Light Mode";
+  } else {
+    themeBtn.textContent = "🌙 Dark Mode";
+  }
+});
